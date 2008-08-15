2857bd04b18441c75422073edd4b0b38b224527e
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-08-15 23:13:41 +0000

    Translated files for portuguesebr with UTF-8
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imtranslating/trunk@4370 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/portuguesebr/admin.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Portuguesebr language constants used in admin section of the module
+*
+* @copyright	The SmartFactory http://www.smartfactory.ca
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @package		Imtranslating
+* @author		marcan <marcan@smartfactory.ca>
+* @version		$Id: admin.php 1394 2008-05-22 16:21:43Z marcan $
+* @translation        GibaPhp
+*/
+
+if (!defined("XOOPS_ROOT_PATH")) {
+ 	die("Caminho Raiz do ImpressCMS não está definido");
+}
+
+
+define("_AM_IMTRANSL_FROM_LANG", "Idioma de Referência");
+define("_AM_IMTRANSL_TO_LANG", "Língua da tradução");
+define("_AM_IMTRANSL_MODULE", "Módulo");
+define("_AM_IMTRANSL_COREFILES", "Arquivos Core/Núcleo");
+define("_AM_IMTRANSL_INSTALLFILES", "Arquivos de Instalação");
+define("_AM_IMTRANSL_GO", "Continua");
+define("_AM_IMTRANSL_CANCEL", "Cancelar");
+define("_AM_IMTRANSL_ORIGINAL", "%s definição para %s");
+define("_AM_IMTRANSL_JOB", "Constante Ausente em %s");
+define("_AM_IMTRANSL_TRANSLATION", "Tradução");
+define("_AM_IMTRANSL_COMMENT", "//Nova constante criada através do IMtranslating");
+define("_AM_IMTRANSL_ZIP", "Zip/Compactar e download");
+define("_AM_IMTRANSL_DONE", "Você Terminou!");
+// Log messages
+define("_AM_IMTRANSL_READ_ERR", "Não é possível encontrar os arquivos de referência.");
+define("_AM_IMTRANSL_PARSE_ERR", "Pasta de Referência não contém os arquivos de idioma.");
+define("_AM_IMTRANSL_NO_TRANS_NEEDED_ERR", "Todas as constantes de idioma já estão definidas.");
+define("_AM_IMTRANSL_WRITE_ERR", "Não é possível escrever no arquivo %s.");
+
+?>
---
+++ language/portuguesebr/modinfo.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+* Portuguesebr language constants used in admin section and user section of the module
+*
+* $Id: modinfo.php 1394 2008-05-22 16:21:43Z marcan $
+* Module: Imtranslating
+* Author: The SmartFactory <www.smartfactory.ca>
+* @translation        GibaPhp
+* Licence: GNU
+*/
+
+/**  not edited by RJB on 3/10/07 */
+
+if (!defined("XOOPS_ROOT_PATH")) {
+ 	die("Caminho Raiz do ImpressCMS não está definido");
+}
+
+// Module Info
+// The name of this module
+
+global $xoopsModule;
+define("_MI_IMTRANSLATING_MD_NAME", "imTranslating");
+define("_MI_IMTRANSLATING_MD_DESC", "Gerenciador de registro e Eventos do ImpressCMS ");
+
+define("_MI_IMTRANSLATING_LOGS", "Logs");
+define("_MI_IMTRANSLATING_TYPES", "Tipos");
+define("_MI_IMTRANSLATING_CATEGORIES", "Categorias");
+
+define("_MI_IMTRANSLATING_TEAM_GR", "Equipes");
+define("_MI_IMTRANSLATING_TEAM_GRDSC", "Estes grupos serão considerados como os usuários de seu time. Estes grupos têm permissão para criar, editar e apagar listas e itens em que o usuário está trabalhando.");
+
+
+?>
