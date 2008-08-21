d293fcc4daae211210b682d49fda11d1711cf909
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-08-21 23:45:00 +0000

    Verification and comparison of files finalised for translation...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imtranslating/trunk@4496 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/admin.php
+++ language/portuguesebr/admin.php
@@ -8,14 +8,16 @@
 * @package		Imtranslating
 * @author		marcan <marcan@smartfactory.ca>
 * @version		$Id: admin.php 1394 2008-05-22 16:21:43Z marcan $
-* @translation        GibaPhp
+* @translation  GibaPhp
 */
 
-if (!defined("XOOPS_ROOT_PATH")) {
- 	die("Caminho Raiz do ImpressCMS não está definido");
-}
+if (!defined("ICMS_ROOT_PATH")) die("Caminho Raiz do ImpressCMS não está definido");
 
-
+define("_AM_IMTRANSL_START_FORM", "Start translating!");
+define("_AM_IMTRANSL_START_FORM_DSC", "Fill out the following form in order to start translating.");
+define("_AM_IMTRANSL_TRANSLATE", "Translate");
+define("_AM_IMTRANSL_FINISH_FORM", "You are done !");
+define("_AM_IMTRANSL_FINISH_FORM_DSC", "Congratulations, you have completed the translation. Please note that imTranslating has created the language files structure under yoursite/uploads/imtranslating/. You can download the files directly from there. Alternatively you can also click on the Zip and Download button below to download an archive version of the new language pack.");
 define("_AM_IMTRANSL_FROM_LANG", "Idioma de Referência");
 define("_AM_IMTRANSL_TO_LANG", "Língua da tradução");
 define("_AM_IMTRANSL_MODULE", "Módulo");
@@ -25,6 +27,7 @@ define("_AM_IMTRANSL_GO", "Continua");
 define("_AM_IMTRANSL_CANCEL", "Cancelar");
 define("_AM_IMTRANSL_ORIGINAL", "%s definição para %s");
 define("_AM_IMTRANSL_JOB", "Constante Ausente em %s");
+define("_AM_IMTRANSL_JOB_INFO", "Please complete the following form in order to update the language constants of %s.");
 define("_AM_IMTRANSL_TRANSLATION", "Tradução");
 define("_AM_IMTRANSL_COMMENT", "//Nova constante criada através do IMtranslating");
 define("_AM_IMTRANSL_ZIP", "Zip/Compactar e download");
--- language/portuguesebr/modinfo.php
+++ language/portuguesebr/modinfo.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
 * Portuguesebr language constants used in admin section and user section of the module
 *
@@ -10,11 +9,8 @@
 * Licence: GNU
 */
 
-/**  not edited by RJB on 3/10/07 */
 
-if (!defined("XOOPS_ROOT_PATH")) {
- 	die("Caminho Raiz do ImpressCMS não está definido");
-}
+if (!defined("ICMS_ROOT_PATH")) die("Caminho Raiz do ImpressCMS não está definido");
 
 // Module Info
 // The name of this module
@@ -23,12 +19,5 @@ global $xoopsModule;
 define("_MI_IMTRANSLATING_MD_NAME", "imTranslating");
 define("_MI_IMTRANSLATING_MD_DESC", "Gerenciador de registro e Eventos do ImpressCMS ");
 
-define("_MI_IMTRANSLATING_LOGS", "Logs");
-define("_MI_IMTRANSLATING_TYPES", "Tipos");
-define("_MI_IMTRANSLATING_CATEGORIES", "Categorias");
-
-define("_MI_IMTRANSLATING_TEAM_GR", "Equipes");
-define("_MI_IMTRANSLATING_TEAM_GRDSC", "Estes grupos serão considerados como os usuários de seu time. Estes grupos têm permissão para criar, editar e apagar listas e itens em que o usuário está trabalhando.");
-
-
+define("_MI_IMTRANSLATING_TRANSLATE", "Translate");
 ?>
