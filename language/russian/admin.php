087ecbc4aa246af8d9e31e3e9a81d54a80485cf2
algalochkin <contact@impresscms.org>
2008-11-11 17:50:20 +0000

    Russian translation of ImTranslating module
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imtranslating/trunk@6738 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/russian/admin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* admin side language constants
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		http://inboxinternational.com INBOX International inc.
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("Не определен корневой путь к ICMS");
+
+define("_AM_IMTRANSL_START_FORM", "Начать перевод!");
+define("_AM_IMTRANSL_START_FORM_DSC", "Для начала перевода заполните следующую форму.");
+define("_AM_IMTRANSL_TRANSLATE", "Перевод");
+define("_AM_IMTRANSL_FINISH_FORM", "Все готово!");
+define("_AM_IMTRANSL_FINISH_FORM_DSC", "Поздравлям, у Вас имеется полный перевод. Пожалуйста, обратите внимание, что imTranslating создает структуру языковых файлов в каталоге yoursite/uploads/imtranslating/. Вы можете загрузить файлы напрямую отсюда. В качестве альтернативы, Вы можете кликнуть на кнопку <b>Zip и загрузить</b> для загрузки архивной версии языкового пакета.");
+define("_AM_IMTRANSL_FROM_LANG", "Базовый язык");
+define("_AM_IMTRANSL_TO_LANG", "Язык для перевода");
+define("_AM_IMTRANSL_MODULE", "Модуль");
+define("_AM_IMTRANSL_COREFILES", "Файлы ядра");
+define("_AM_IMTRANSL_INSTALLFILES", "Файлы инсталляции");
+define("_AM_IMTRANSL_GO", "Продолжить");
+define("_AM_IMTRANSL_CANCEL", "Отменить");
+define("_AM_IMTRANSL_ORIGINAL", "%s определение для %s");
+define("_AM_IMTRANSL_JOB", "Недостающие константы в %s");
+define("_AM_IMTRANSL_JOB_INFO", "Пожалуйста, заполните следующую форму для обновления языковых констант в %s.");
+define("_AM_IMTRANSL_TRANSLATION", "Перевод");
+define("_AM_IMTRANSL_COMMENT", "//Новые константы созданы в модуле ImTranslating");
+define("_AM_IMTRANSL_ZIP", "Zip и загрузить");
+define("_AM_IMTRANSL_DONE", "Все готово!");
+// Log messages
+define("_AM_IMTRANSL_READ_ERR", "Невозможно найти необходимые файлы.");
+define("_AM_IMTRANSL_PARSE_ERR", "Каталог не содержит языковых файлов.");
+define("_AM_IMTRANSL_NO_TRANS_NEEDED_ERR", "Все языковые константы определены.");
+define("_AM_IMTRANSL_WRITE_ERR", "Невозможно записать в файл %s.");
+
+?>
---
+++ language/russian/modinfo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* modinfo language constants
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		http://inboxinternational.com INBOX International inc.
+* @version		$Id$ Russian translation. Charset: utf-8 (without BOM)
+*/
+
+
+if (!defined("ICMS_ROOT_PATH")) die("Не определен корневой путь к ICMS");
+
+// Module Info
+// The name of this module
+
+global $xoopsModule;
+define("_MI_IMTRANSLATING_MD_NAME", "imTranslating");
+define("_MI_IMTRANSLATING_MD_DESC", "Управление переводом констант в ImpressCMS");
+
+define("_MI_IMTRANSLATING_TRANSLATE", "Перевод");
+?>
