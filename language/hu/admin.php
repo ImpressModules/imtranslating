92d2411bada2d3e9af43745d478801ca64d630fd
David Janssens <fiammybe@gmail.com>
2025-06-13 16:35:44 +0200

    New translations admin.php (Hungarian)

---
+++ language/hu/admin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* admin side language constants
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		http://inboxinternational.com INBOX International inc.
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+define("_AM_IMTRANSL_START_FORM", "Start translating!");
+define("_AM_IMTRANSL_START_FORM_DSC", "Fill out the following form in order to start translating.");
+define("_AM_IMTRANSL_TRANSLATE", "Translate");
+define("_AM_IMTRANSL_FINISH_FORM", "You are done !");
+define("_AM_IMTRANSL_FINISH_FORM_DSC", "Congratulations, you have completed the translation. Please note that imTranslating has created the language files structure under yoursite/uploads/imtranslating/. You can download the files directly from there. Alternatively you can also click on the Zip and Download button below to download an archive version of the new language pack.");
+define("_AM_IMTRANSL_FROM_LANG", "Base language");
+define("_AM_IMTRANSL_TO_LANG", "Language of the translation");
+define("_AM_IMTRANSL_MODULE", "Module");
+define("_AM_IMTRANSL_COREFILES", "Core Files");
+define("_AM_IMTRANSL_INSTALLFILES", "Installation Files");
+define("_AM_IMTRANSL_GO", "Continue");
+define("_AM_IMTRANSL_CANCEL", "Cancel");
+define("_AM_IMTRANSL_ORIGINAL", "%s definition for %s");
+define("_AM_IMTRANSL_JOB", "Missing constant in %s");
+define("_AM_IMTRANSL_JOB_INFO", "Please complete the following form in order to update the language constants of %s.");
+define("_AM_IMTRANSL_TRANSLATION", "Translation");
+define("_AM_IMTRANSL_COMMENT", "//New constants created via IMtranslating");
+define("_AM_IMTRANSL_ZIP", "Zip and download");
+define("_AM_IMTRANSL_DONE", "You're Done!");
+// Log messages
+define("_AM_IMTRANSL_READ_ERR", "Cannot find the reference files.");
+define("_AM_IMTRANSL_PARSE_ERR", "Reference folder doesn't contains language files.");
+define("_AM_IMTRANSL_NO_TRANS_NEEDED_ERR", "All language constants are already defined.");
+define("_AM_IMTRANSL_WRITE_ERR", "Cannot write in fille %s.");
+
+?>
